626a21bb7572bce7a9db9e3f37bc5fa30bd42c4d
jjeong <bigi5@naver.com>
2019-12-02 12:14:15 +0900

    Python Mysql connection pool

--- connection_pool/connection_pool_main.py
+++ connection_pool/connection_pool_main.py
@@ -1,5 +1,6 @@
 import inspect
 from connection_pool.connection_pool_study import DatabaseConnectionPool
+from connection_pool.connection_pool_study02 import ExplicitlyConnectionPool, get_implicitly_connection
 
 
 def connect_pool01():
@@ -9,6 +10,30 @@ def connect_pool01():
     connection.close()
 
 
+def explicitly_connection_pool():
+    print("\n== {}() ==".format(inspect.stack()[0][3]))
+    connectionPool = ExplicitlyConnectionPool.get_instance()
+    print("ConnectionPool {}".format(connectionPool))
+    connection = connectionPool.get_connection()
+    print("Connection {}".format(connection))
+    connection.close()
+
+
+def implicitly_connection_pool():
+    print("\n== {}() ==".format(inspect.stack()[0][3]))
+    connectionPool = get_implicitly_connection()
+    print("ConnectionPool {}".format(connectionPool))
+    connection = connectionPool.get_connection()
+    print("Connection {}".format(connection))
+    connection.close()
+
+
 if __name__ == '__main__':
-    for i in range(10):
-        connect_pool01()
+    # for i in range(10):
+    #     connect_pool01()
+
+    explicitly_connection_pool()
+    implicitly_connection_pool()
+
+    explicitly_connection_pool()
+    implicitly_connection_pool()
